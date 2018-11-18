817d38a652d87e01f158e51e8df70d27b1d60dca
zhang0peter <zsd780885029@gmail.com>
2018-11-18 22:18:52 +0800

    fix bug

--- pi-monitor-flask.py
+++ pi-monitor-flask.py
@@ -5,34 +5,33 @@ from flask import Flask, render_template, url_for
 from datetime import timedelta
 import os
 
-
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(
     seconds=60)  # 设置图片的缓存时间为1分钟
 
 
 def get_temperature():
-    global conn
+    conn = sqlite3.connect('data.db')
     temperature = conn.execute(
         "select temperature,time from temperature  ;").fetchall()
     return temperature
 
 
 def get_mem():
-    global conn
+    conn = sqlite3.connect('data.db')
     MemTotal = os.popen(
-            "cat /proc/meminfo | grep MemTotal |awk  '{print $2 / 1024}'").readline()
+        "cat /proc/meminfo | grep MemTotal |awk  '{print $2 / 1024}'").readline()
     MemTotal = float(MemTotal)
     mem = conn.execute(
         "select mem,time from mem  ;").fetchall()
-    return mem,MemTotal
+    return mem, MemTotal
 
 
 @app.route('/', methods=['GET'])
 def main():
     temperature = get_temperature()
-    mem,MemTotal = get_mem()
-    return render_template('index.html', temperature=temperature, mem=mem,MemTotal=MemTotal)
+    mem, MemTotal = get_mem()
+    return render_template('index.html', temperature=temperature, mem=mem, MemTotal=MemTotal)
 
 
 @app.route('/mem', methods=['GET'])
@@ -48,6 +47,4 @@ def cpu():
 
 
 if __name__ == '__main__':
-    global conn
-    conn = sqlite3.connect('data.db')
     app.run(host='0.0.0.0', port=4000, debug=True)
